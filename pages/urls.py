f7d064ee7b1844082ae9d7fd0bcd4512a9f74679
KevinCabee <kcabrera913@gmail.com>
2024-07-15 16:57:55 -0700

    Assignment 2 113

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,5 @@ from pages import views
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("about/", views.AboutPageView.as_view(), name="about"),
+    # path("list/", views.ListPageView.as_view(), name="list"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,22 +5,32 @@ from django.views.generic import (
     DeleteView,
     ListView
 )
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, 
+    UserPassesTestMixin
+    )
+
 from django.urls import reverse_lazy
-from .model import Post
+from .models import Post
 
 
 class PostCreateView(CreateView):
     template_name = "posts/new.html"
-    model = Postfields = ['title', 'subtitle', 'body', 'author', 'status']
+    model = Post
+    fields = ['title', 'subtitle', 'body', 'author', 'status']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class PostDetailView(DetailView):
         template_name = "posts/detail.html"
         model = Post
 
 class PostUpdateView(UpdateView):
-            template_name = "posts/edit.html"
-            model = Post
-            fields = ['title', 'subtitle', 'body', 'status']
+        template_name = "posts/edit.html"
+        model = Post
+        fields = ['title', 'subtitle', 'body', 'status']
 
 class PostDeleteView(DeleteView):
         template_name = "posts/delete.html"
